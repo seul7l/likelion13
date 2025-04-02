c652450ced2a4e499b469fa608e264242e4241ec
seulgi han <chiaki2c@skuniv.ac.kr>
2025-04-02 16:52:20 +0900

    2주차 과제

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ export default function App() { //React 컴포넌트를 내보내는 코드
   return (
     <div>
       <header className="header">
-        <h1>My profile</h1>
-        <p>나만의 웹사이트를 만들어봐요! </p>
+        <h1>슬기로운 멋사생활</h1>
+        <p>한슬기 프로필</p>
       </header>
        
     <nav className="nav">
@@ -19,10 +19,10 @@ export default function App() { //React 컴포넌트를 내보내는 코드
 
     <main className="main">
         <section id="about" className="section">
-            <h2>안녕하세요!</h2>
-            <p>만나서 반갑습니다. 저는 컴퓨터공학과에 재학중인 22학번 홍길동입니다.</p>
-            <p>함께 즐거운 동아리 생활 보내봐요 ^^</p>
-            <img className="img" src="https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/eViY/image/is3UFbmo7Gwuiln43EznmVZFwXo.jpg" alt="about_image">
+            <h2>안녕하세요 ^~^</h2>
+            <p>만나서 반갑습니다. 저는 미래융합학부1에 재학중인 25학번 한슬기입니다.</p>
+            <p>함께 즐거운 동아리 생활 보내봐요!</p>
+            <img className="img" src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDEyMDlfMjYy%2FMDAxNzMzNzIyNzgyMTky.L5-limQHy33dmJljwc92uxcRyplDQne-F3utQzNqQpcg.vZLlps2xW_7r0smtPdlHcsnXv9swLePPyZcZyetWaq4g.JPEG%2FIMG_2816.JPG&type=sc960_832" alt="hello_image">
             </img>
         </section>
 
@@ -31,28 +31,27 @@ export default function App() { //React 컴포넌트를 내보내는 코드
             
             <h3>취미</h3>
             <ul>
-                <li>농구 보기</li>
-                <li>음악 감상</li>
-                <li>기타 치기</li>
-                <li>코딩 ^^</li>
+                <li>드라마, 영화 보기</li>
+                <li>소설책 읽기</li>
+                <li>수영</li>
             </ul>
-            <img className="img" src="https://i.ytimg.com/vi/dIt_N7yNTgk/mqdefault.jpg" alt="guitar_image">
+            <img className="img" src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA1MjFfMjE2%2FMDAxNjIxNTcyODgxNzQ4.Ielxn5BEEtP1AN7-NpEYAE0aFXQSbTcCTGCrMyvWyJEg.HdWJpaXF1sFVwOW5wPgALnTooLaAR609ta9b10dygYAg.JPEG.xoxo_0717%2FIMG_4155.JPG&type=sc960_832" alt="book_image">
             </img>
 
             <h3>좋아하는 음식 순위</h3>
             <ol>
-                <li>타코</li>
                 <li>떡볶이</li>
-                <li>햄버거</li>
+                <li>마라탕</li>
+                <li>삼겹살</li>
             </ol>
-            <img className="img" src="https://i.pinimg.com/474x/7a/9e/ec/7a9eec73158b3e0a84691551180fef8e.jpg" alt="hamburger_image">
+            <img className="img" src="https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODEwMjVfODIg%2FMDAxNTQwNDMzMjU0Mjk3.3yVdZ6Td8NkbzHq-CS2b6GZI0vYBvN4QiZ4F544t0Osg.cDkyKs3BjQ8zCNqieBmhGu8Nzs7IvcNUGJxnFinPHJgg.JPEG.tllros2ss%2FKakaoTalk_20181023_184601536.jpg&type=sc960_832" alt="food_image">
             </img>
         </section>
 
         <section id="contact" className="section">
             <h2>연락하기</h2>
-            <p>이메일: skulikelion@example.com</p>
-            <p>전화번호: 010-1234-5678</p>
+            <p>이메일: chiaki2c@skuniv.ac.kr</p>
+            <p>전화번호: 010-7358-4092</p>
         </section>
     </main>
 
