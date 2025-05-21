ff6c62df784295244b153aa164a5a339b320cef5
Seulgi Han <chiaki2c@skuniv.ac.kr>
2025-05-22 03:23:27 +0900

    page1

---
+++ week5/src/components/header/Header.styles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+
+export const HeaderWrapper = styled.header`
+  padding: 20px 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Logo = styled.h1`
+  color: #1c1c1c;
+  text-align: center;
+  font-size: 70px;
+  font-family: "TT Travels Next Trl";
+  font-style: normal;
+  font-weight: 700;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  gap: 20px;
+  font-size: 25px; 
+`;
+
+export const NavItem = styled.div`
+  color: #a7a7a7;
+  font-weight: 500;
+  margin: 10px;
+  cursor: pointer;
+
+  &.active {
+    color: black;
+    font-weight: bold;
+    border-bottom: 2px solid black;
+  }
+`;
---
+++ week5/src/routes/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+// import ProjectPage from "../pages/ProfectPage";
+
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/project" element={ <ProjectPage /> } /> */}
+        {/* 향후 다른 페이지도 여기에 추가 가능 */}
+      </Routes>
+    </Router>
+  );
+}
+// 프로젝트 페이지를 만들어서 클릭하면 그 페이지로 연결되도록
